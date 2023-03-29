4f8a73e72d58ce94726b137b67d97a441bb9544d
nazar5444 <nazar24t@gmail.com>
2023-03-29 19:58:43 +0300

    alert-fix

--- alertlive.py
+++ alertlive.py
@@ -1,7 +1,5 @@
 import json
-
 from aiohttp_sse_client import client as sse_client
-
 import db
 
 
@@ -9,24 +7,24 @@ async def alertlive_func():
     headers = {"X-API-Key": "5ebcb2ed4d4fd3d565a3d4ae028c0242c5e583d8"}
     async with sse_client.EventSource(
             'https://alerts.com.ua/api/states/live',
-            headers=headers
+            headers=headers, timeout=-1
     ) as event_source:
         try:
             async for event in event_source:
                 if event.type == "hello":
-                    print(event.data)
+                    print("SSE Conected sucsessfully")
                 if event.type == "update":
                     data = json.loads(event.data)
                     state = data["state"]
                     print("State ID:", state["id"])
                     print("Alert:", state["alert"])
-                    print(event)
-                    res = db.cur.execute("SELECT user_id FROM users WHERE city_id=%s", (state["id"],))
+                    res = db.cur.execute("SELECT user_id FROM users WHERE city_id::text = %s", (str(state["id"]),))
                     id_list_changes = db.cur.fetchall()
-                    for user_id in id_list_changes:
-                        if state["alert"] is False:
-                            print(user_id[0], 'Відбій повітряної тривоги')
-                        elif state["alert"] is True:
-                            print(user_id[0], 'Повітряна тривога')
+                    if id_list_changes:
+                        for user_id in id_list_changes:
+                            if state["alert"] is False:
+                                print(user_id[0], 'Відбій повітряної тривоги')
+                            elif state["alert"] is True:
+                                print(user_id[0], 'Повітряна тривога')
         except ConnectionError:
             pass
